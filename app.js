230d18e4279f6a134018fcea035e796a9d21c0bd
nicorey89 <nicolasthian8907@gmail.com>
2023-01-05 04:39:47 -0300

    agregando formularios

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
 
 app.listen(PORT, () => 
     console.log("servidor levantado en el puerto " + PORT))
